d5026d566b7999be1e033f92fab4ace90d6c88c1
Seba Contreras <sacg93@gmail.com>
2021-10-23 10:39:40 +0200

    Splitting the contact matrix into different npi-sensitive matrixes

--- Intento.m
+++ Intento.m
@@ -22,6 +22,7 @@ C1_our = zeros(6,6);
 C2_our = zeros(6,6);
 C3_our = zeros(6,6);
 C4_our = zeros(6,6);
+Mi_our = zeros(6,1);
 for i = 1:6
     for j = 1:6
         C1_our(i,j) = sum(C1(ind(i):ind(i+1),ind(j):ind(j+1)),'all');
@@ -33,9 +34,24 @@ end
 
 
 CT = C1_our*w.Household+C2_our*w.School+C3_our*w.Work+C4_our*w.General;
-C_vol = C4_our*w.General+0.3*C3_our*w.Work;
+rho = 1/max(eig(CT));
+CT = rho*(CT);
+%C_vol = 0.7*C4_our*w.General+0.2*C3_our*w.Work+0.1*C2_our*w.School;
 
+C_vol           = rho*((1/3) * C3_our*w.Work + (1/2) * C4_our*w.General);
+C_NPI_work      = rho*((1/3) * C3_our*w.Work);
+C_NPI_School    = rho*(C2_our*w.School);
+C_NPI_general   = rho*((1/2) * C4_our*w.General);
+C_Base          = rho*(C1_our*w.Household +(1/3) * C3_our*w.Work);
+
+
+%% check
+
+CT == C_vol+C_NPI_work+C_NPI_School+C_NPI_general+C_Base
 %% Percentage of contacts that are reductible
 
-p = CT./C_vol;
-writematrix(p,'reductible_contacts.csv') 
+writematrix(C_vol,'C_vol.csv') 
+writematrix(C_NPI_work,'C_NPI_work.csv') 
+writematrix(C_NPI_School,'C_NPI_School.csv') 
+writematrix(C_NPI_general,'C_NPI_general.csv') 
+writematrix(C_Base,'C_Base.csv') 
